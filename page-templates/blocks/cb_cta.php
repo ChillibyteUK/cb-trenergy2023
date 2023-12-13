f925906af6458cfa66c6b107672606d9bb1f7dd8
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-13 15:07:26 +0000

    Update cb_cta.php

--- page-templates/blocks/cb_cta.php
+++ page-templates/blocks/cb_cta.php
@@ -1,26 +1,11 @@
 <?php
-if (null !== get_field('theme')) {
-    if (get_field('theme') == 'Pods') {
-        $logo = 'timberrooms-logo-prefab--wo.svg';
-    }
-    elseif (get_field('theme') == 'Golf') {
-        $logo = 'timberrooms-logo-golf--wo.svg';
-    }
-    else {
-        $logo = 'timberrooms-logo--wo.svg';
-    }
-}
-else {
-    $logo = 'timberrooms-logo--wo.svg';
-}
-
-// $theme = get_field('theme') == 'Pods' ? 'has-prefab-color' : 'has-primary-color';
+$logo = 'tr-energy-logo--wo.svg';
 $theme = '';
 ?>
 <section class="cta">
     <div class="container-xl text-center" data-aos="fade">
         <img src="<?=get_stylesheet_directory_uri()?>/img/<?=$logo?>" alt="">
-        <h3>Are you considering a Garden Room?</h3>
+        <h3><?=get_field('title')?></h3>
         <a href="/contact/" class="h5 <?=$theme?>">Book a Free Site Survey</a>
         <div class="cta__grid">
             <div class="line"></div>
