3316cf837d0028bf7eb6ee87f3164879b348d82b
CheechRockwizard <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-19 15:54:16 +0000

    Update cb_cta.php

--- page-templates/blocks/cb_cta.php
+++ page-templates/blocks/cb_cta.php
@@ -4,12 +4,16 @@ $theme = '';
 ?>
 <section class="cta">
     <div class="container-xl text-center" data-aos="fade">
-        <img src="<?=get_stylesheet_directory_uri()?>/img/<?=$logo?>" alt="">
+        <img src="<?=get_stylesheet_directory_uri()?>/img/<?=$logo?>"
+            alt="">
         <h3><?=get_field('title')?></h3>
-        <a href="/contact/" class="h5 <?=$theme?>">Book a Free Site Survey</a>
+        <a href="/contact-us/" class="h5 <?=$theme?>">Book a Free
+            Site Survey</a>
         <div class="cta__grid">
             <div class="line"></div>
-            <div class="h5">Call Now: <a href="tel:<?=parse_phone(get_field('contact_phone','options'))?>"><?=get_field('contact_phone','options')?></a></div>
+            <div class="h5">Call Now: <a
+                    href="tel:<?=parse_phone(get_field('contact_phone', 'options'))?>"><?=get_field('contact_phone', 'options')?></a>
+            </div>
             <div class="line"></div>
         </div>
     </div>
